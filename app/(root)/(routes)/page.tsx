4d41093636308b2b66b81e3eb746604eaf274608
James Chen <jameschen09@yahoo.com>
2024-03-29 16:13:29 -0700

    inital commit with auth

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+const RootPage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+      Build me James. AI DEMANDS IT!! 
+    </div>
+  )
+}
+
+export default RootPage
