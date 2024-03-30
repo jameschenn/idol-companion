b64256b1ec605c2f007b4f4788d5c5488fab30f7
James Chen <jameschen09@yahoo.com>
2024-03-29 21:15:39 -0700

    navbars first pass

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { UserButton } from '@clerk/nextjs'
+
 const RootPage = () => {
   return (
     <div>
-      <UserButton afterSignOutUrl='/' />
       Build me James. AI DEMANDS IT!! 
     </div>
   )
